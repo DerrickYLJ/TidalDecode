95a9941bfa0038e62502b4b5f7d3a3bf08a2eebd
DerrickYLJ <lijiey@andrew.cmu.edu>
2024-07-17 18:15:05 -0400

    Incorporate top-k index

--- src/index_build/modify_llama.py
+++ src/index_build/modify_llama.py
@@ -23,36 +23,25 @@ class KeyValueIndexStore:
         self.key_index_store = [
             faiss.IndexFlatL2(dimension) for _ in range(num_kv_heads)  # on CPU
         ]
-        self.value_index_store = [
-            faiss.IndexFlatL2(dimension) for _ in range(num_kv_heads)  # on CPU
-        ]
         self.top_k = top_k
         self.past_key_value = prefilling
     
     def update_kv_cache(self, key_states, value_states):
         self.past_key_value[0] = torch.cat([self.past_key_value[0], key_states], dim=2)
         self.past_key_value[1] = torch.cat([self.past_key_value[1], value_states], dim=2)
-        return (self.past_key_value[0], self.past_key_value[1])
 
-    def update_index_store(self, key_states, value_states):
+    def update_index_store(self, key_states):
         # key_states and value_states are of shape (bsz, num_key_value_heads, q_len, head_dim)
         bsz, num_kv_heads, q_len, head_dim = key_states.size()
         if num_kv_heads != self.num_kv_heads:
             raise ValueError(
                 f"dimension of key_states when updating index store is wrong: should be {self.num_kv_heads} but got {num_kv_heads}"
             )
-        if key_states.size() != value_states.size():
-            raise ValueError(
-                f"mismatch dimension of key_states and value_states when updating index store"
-            )
         for head in range(self.num_kv_heads):
             # Convert to numpy for FAISS
             keys = key_states[:, head, :, :].reshape(-1, head_dim).numpy()
-            values = value_states[:, head, :, :].reshape(-1, head_dim).numpy()
             keys = np.ascontiguousarray(keys).astype(np.float32)
-            values = np.ascontiguousarray(values).astype(np.float32)
             self.key_index_store[head].add(keys)
-            self.value_index_store[head].add(values)
 
     def search_index_store(self, query_states):
         if self.top_k is None:
@@ -69,7 +58,6 @@ class KeyValueIndexStore:
             D_k, I_k = self.key_index_store[i_index].search(queries, k=self.top_k)
             sorted_indices = np.argsort(I_k, axis=1)
             sorted_I_k = np.take_along_axis(I_k, sorted_indices, axis=1)
-            # D_v, I_v = self.value_index_store[i_index].search(queries, k=self.top_k)
             keys_retrieved = self.past_key_value[0][:, i_index, :, :].reshape(-1, head_dim)[sorted_I_k].reshape(bsz, self.top_k, head_dim)
             values_retrieved = self.past_key_value[1][:, i_index, :, :].reshape(-1, head_dim)[sorted_I_k].reshape(bsz, self.top_k, head_dim)
             retrieved_k.append(keys_retrieved)
@@ -144,7 +132,7 @@ def llama_index_build_attention_forward(
     value_states_cpu = value_states.to("cpu")
 
     if past_key_value is not None:
-        key_states, value_states = self.kv_index_store.update_kv_cache(key_states_cpu, value_states_cpu)
+        self.kv_index_store.update_kv_cache(key_states_cpu, value_states_cpu)
         if key_states.size() != value_states.size():
             raise ValueError(
                 f"key_states and value_states are mismatched, key_states: {key_states.size()} but value_states: {value_states.size()}"
@@ -156,7 +144,7 @@ def llama_index_build_attention_forward(
         )
         past_key_value = (key_states_cpu, value_states_cpu)
 
-    self.kv_index_store.update_index_store(key_states_cpu, value_states_cpu)
+    self.kv_index_store.update_index_store(key_states_cpu)
 
     # select top_k keys and values from kv_cache based on index_store
     if top_k is not None and q_len == 1:
@@ -167,7 +155,6 @@ def llama_index_build_attention_forward(
         key_states = key_states.to(hidden_states.device)
         value_states = value_states.to(hidden_states.device)
     else:
-        key_states, value_states = key_states.to(hidden_states.device), value_states.to(hidden_states.device)
         key_states = repeat_kv(key_states, self.num_key_value_groups)
         value_states = repeat_kv(value_states, self.num_key_value_groups)
         
@@ -230,7 +217,6 @@ def llama_index_build_attention_forward(
 
     if not output_attentions:
         attn_weights = None
-    # print(f"{self.layer_idx}: {kv_seq_len}: hidden_states: {hidden_states.device}; query_states: {query_states.device}; key_states: {key_states.device}; attn_output: {attn_output.device}")
     return attn_output, attn_weights, past_key_value
 
 def llama_index_build_dist_attention_forward(
@@ -280,42 +266,41 @@ def llama_index_build_dist_attention_forward(
     cos, sin = self.rotary_emb(value_states, seq_len=kv_seq_len)
     query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin, position_ids)
 
+    # update index_store + kv_cache
+    key_states_cpu = key_states.to("cpu")
+    value_states_cpu = value_states.to("cpu")
+
     if past_key_value is not None:
         # reuse k, v, self_attention
-        key_states = torch.cat([past_key_value[0], key_states], dim=2)
-        value_states = torch.cat([past_key_value[1], value_states], dim=2)
+        self.kv_index_store.update_kv_cache(key_states_cpu, value_states_cpu)
+        if key_states.size() != value_states.size():
+            raise ValueError(
+                f"key_states and value_states are mismatched, key_states: {key_states.size()} but value_states: {value_states.size()}"
+            )
+        past_key_value = (self.kv_index_store.past_key_value[0], self.kv_index_store.past_key_value[1])
+    else: 
+        self.kv_index_store = KeyValueIndexStore(
+            self.head_dim, self.num_key_value_heads, top_k, [key_states_cpu, value_states_cpu]
+        )
+        past_key_value = (key_states_cpu, value_states_cpu)
 
-    past_key_value = (key_states, value_states) if use_cache else None
-    # repeat k/v heads if n_kv_heads < n_heads
-    key_states = repeat_kv(key_states, self.num_key_value_groups)
-    value_states = repeat_kv(value_states, self.num_key_value_groups)
+    self.kv_index_store.update_index_store(key_states_cpu)
 
-    # attn_weights = torch.matmul(query_states, key_states.transpose(2, 3)) / math.sqrt(self.head_dim)
-    # if attn_weights.size() != (bsz, self.num_heads, q_len, kv_seq_len):
-    #     raise ValueError(
-    #         f"Attention weights should be of size {(bsz, self.num_heads, q_len, kv_seq_len)}, but is"
-    #         f" {attn_weights.size()}"
-    #     )
-    # attn_weights_ground_truth_1 = attn_weights
-    # if attention_mask is not None:
-    #     if attention_mask.size() != (bsz, 1, q_len, kv_seq_len):
-    #         raise ValueError(
-    #             f"Attention mask should be of size {(bsz, 1, q_len, kv_seq_len)}, but is {attention_mask.size()}"
-    #         )
-    #     attn_weights_ground_truth_2 = attn_weights + attention_mask
 
-    # attn_weights_ground_truth_3 = nn.functional.softmax(attn_weights_ground_truth_2, dim=-1, dtype=torch.float32).to(query_states.dtype)
+    # select top_k keys and values from kv_cache based on index_store
+    if top_k is not None and q_len == 1:
+        # retrieve top-k in the index store
+        key_states, value_states = self.kv_index_store.search_index_store(
+            query_states.to("cpu")
+        )
+        key_states = key_states.to(default_device)
+        value_states = value_states.to(default_device)
+    else:
+        key_states = repeat_kv(key_states, self.num_key_value_groups)
+        value_states = repeat_kv(value_states, self.num_key_value_groups)
 
-    # attn_weights_ground_truth_4 = torch.matmul(attn_weights_ground_truth_3, value_states)
-
-    # if attn_weights_ground_truth_4.size() != (bsz, self.num_heads, q_len, self.head_dim):
-    #     raise ValueError(
-    #         f"`attn_output` should be of size {(bsz, self.num_heads, q_len, self.head_dim)}, but is"
-    #         f" {attn_output.size()}"
-    #     )
-    # attn_weights_ground_truth_4 = attn_weights_ground_truth_4.transpose(1, 2).contiguous()
-    print(query_states.shape[-2])
     if query_states.shape[-2] > 1:
+        # Initialization
         # split keys
         key_states_split = [key_states.chunk(device_count, dim=-2)[i].to(f'cuda:{i}') for i in range(device_count)]
         attention_mask_split = [attention_mask.chunk(device_count, dim=-2)[i].to(f'cuda:{i}') for i in range(device_count)]if attention_mask is not None else [None] * device_count
@@ -341,10 +326,6 @@ def llama_index_build_dist_attention_forward(
             reordered_attn_weights.append(torch.cat(reordered_subchunks, dim=-1))
             for subchunk in reordered_subchunks:
                 del subchunk  # Free up memory
-        # ### test 1 ###
-        # attn_weights_test_1 = torch.cat([reordered.to(default_device) for reordered in reordered_attn_weights], dim=-2)
-        # assert(torch.equal(attn_weights_test_1, attn_weights_ground_truth_1), "test 1")
-        # ### test 1 ###
 
         # attention mask
         attn_weights_masked = []
@@ -355,10 +336,6 @@ def llama_index_build_dist_attention_forward(
                 attn_weights_masked.append(attn_weights_with_mask)
         for i in range(device_count):
             del reordered_attn_weights[0]
-        # ### test 2 ###
-        # attn_weights_test_2 = torch.cat([masked.to(default_device) for masked in attn_weights_masked], dim=-2)
-        # assert torch.equal(attn_weights_test_2, attn_weights_ground_truth_2), "test 2"
-        # ### test 2 ###
 
         # split softmax
         attn_weights_softmax = []
@@ -368,10 +345,6 @@ def llama_index_build_dist_attention_forward(
                 attn_weights_softmax.append(softmax_chunk)
         for i in range(device_count):
             del attn_weights_masked[0]
-        # ### test 3 ###
-        # softmax_test_3 = torch.cat([soft.to(default_device) for soft in attn_weights_softmax], dim=-2)
-        # assert torch.equal(softmax_test_3, attn_weights_ground_truth_3), "test 3"
-        # ### test 3 ###
         
         # attention output
         attn_output = []
@@ -384,25 +357,41 @@ def llama_index_build_dist_attention_forward(
         for i in range(device_count):
             del attn_weights_softmax[0]
         attn_output = torch.cat(attn_output, dim=2).transpose(1, 2).contiguous()
-
-        # ### test 4 ###
-        # assert torch.equal(attn_output, attn_weights_ground_truth_4), "test 4"
-        # ### test 4 ###
     else:
-        
+        # Generation
         attn_weights = torch.matmul(query_states, key_states.transpose(2, 3)) / math.sqrt(self.head_dim)
-        if attn_weights.size() != (bsz, self.num_heads, q_len, kv_seq_len):
-            raise ValueError(
-                f"Attention weights should be of size {(bsz, self.num_heads, q_len, kv_seq_len)}, but is"
-                f" {attn_weights.size()}"
-            )
+        if top_k is not None:
+            if q_len > 1:
+                if attn_weights.size() != (bsz, self.num_heads, q_len, kv_seq_len):
+                    raise ValueError(
+                        f"Attention weights should be of size {(bsz, self.num_heads, q_len, kv_seq_len)}, but is"
+                        f" {attn_weights.size()}"
+                    )
 
-        if attention_mask is not None:
-            if attention_mask.size() != (bsz, 1, q_len, kv_seq_len):
+                if attention_mask is not None:
+                    if attention_mask.size() != (bsz, 1, q_len, kv_seq_len):
+                        raise ValueError(
+                            f"Attention mask should be of size {(bsz, 1, q_len, kv_seq_len)}, but is {attention_mask.size()}"
+                        )
+                    attn_weights = attn_weights + attention_mask
+            else:
+                if attn_weights.size() != (bsz, self.num_heads, q_len, top_k):
+                    raise ValueError(
+                        f"Attention weights should be of size {(bsz, self.num_heads, q_len, top_k)}, but is"
+                        f" {attn_weights.size()}"
+                    )
+        else:
+            if attn_weights.size() != (bsz, self.num_heads, q_len, kv_seq_len):
                 raise ValueError(
-                    f"Attention mask should be of size {(bsz, 1, q_len, kv_seq_len)}, but is {attention_mask.size()}"
+                    f"Attention weights should be of size {(bsz, self.num_heads, q_len, kv_seq_len)}, but is"
+                    f" {attn_weights.size()}"
                 )
-            attn_weights = attn_weights + attention_mask
+            if attention_mask is not None:
+                if attention_mask.size() != (bsz, 1, q_len, kv_seq_len):
+                    raise ValueError(
+                        f"Attention mask should be of size {(bsz, 1, q_len, kv_seq_len)}, but is {attention_mask.size()}"
+                    )
+                attn_weights = attn_weights + attention_mask
 
         # upcast attention to fp32
         attn_weights = nn.functional.softmax(attn_weights, dim=-1, dtype=torch.float32).to(query_states.dtype)
@@ -426,7 +415,6 @@ def llama_index_build_dist_attention_forward(
 
     if not output_attentions:
         attn_weights = None
-
     return attn_output, attn_weights, past_key_value
 
 
